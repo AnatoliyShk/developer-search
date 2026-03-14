f5d56f347dbe35724a72f51a1433e6f007a97971
Anatoliy Shekera <tolikshekera@gmail.com>
2026-03-14 15:07:35 +0200

    remove sqlite from repo

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return render(request, 'projects.html')
+    return render(request, 'projects/projects.html')
 
 def project_detail(request, project_id):
-    return render(request, 'project_detail.html', {'project_id': project_id})
+    return render(request, 'projects/project_detail.html', {'project_id': project_id})
