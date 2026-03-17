66d5efc44a391ad220785b746d77a16eef536ce2
Anatoliy Shekera <tolikshekera@gmail.com>
2026-03-18 01:44:16 +0200

    Feature image

--- projects/views.py
+++ projects/views.py
@@ -15,7 +15,7 @@ def project_detail(request, pk):
 
 def create_project(request):
     if request.method == "POST":
-        form = ProjectForm(request.POST)
+        form = ProjectForm(request.POST,request.FILES)
         if form.is_valid():
             form.save()
             return redirect('projects')
@@ -26,7 +26,7 @@ def create_project(request):
 def update_project(request, pk):
     project = Project.objects.get(id=pk)
     if request.method == "POST":
-        form = ProjectForm(request.POST, instance=project)
+        form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid():
             form.save()
             return redirect('projects')
