08acc97634b6af3722b4426320cb16ad33127760
Anatoliy Shekera <tolikshekera@gmail.com>
2026-03-23 22:37:57 +0200

    Profile fields

--- users/models.py
+++ users/models.py
@@ -3,3 +3,8 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(max_length=254, blank=True)
+    short_intro = models.CharField(max_length=200, blank=True)
+    bio = models.TextField(blank=True)
+    profile_image = models.ImageField(upload_to='profiles/', blank=True, null=True)
