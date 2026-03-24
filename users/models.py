d6c69a799a0b695b3cb25c49dacaa55faf30f139
Anatoliy Shekera <tolikshekera@gmail.com>
2026-03-24 22:29:45 +0200

    Profile link

--- users/models.py
+++ users/models.py
@@ -7,4 +7,13 @@ class Profile(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     short_intro = models.CharField(max_length=200, blank=True)
     bio = models.TextField(blank=True)
-    profile_image = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    profile_image = models.ImageField(upload_to='profiles/', blank=True, null=True, default='profiles/user-default.jpg')
+    social_github = models.URLField(max_length=200, blank=True)
+    social_twitter = models.URLField(max_length=200, blank=True)
+    social_linkedin = models.URLField(max_length=200, blank=True)
+    social_youtube = models.URLField(max_length=200, blank=True)
+    social_website = models.URLField(max_length=200, blank=True)
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+
+    def __str__(self):
+        return str(self.user.username)
