c1767dc80ff4ec8da00b8b2c45d45179df180fbd
Anatoliy Shekera <tolikshekera@gmail.com>
2026-03-30 23:30:29 +0300

    Skills

--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ import uuid
 class Profile(models.Model):
     user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
     name = models.CharField(max_length=100, blank=True)
+    location = models.CharField(max_length=200, blank=True)
     email = models.EmailField(max_length=254, blank=True)
     short_intro = models.CharField(max_length=200, blank=True)
     bio = models.TextField(blank=True)
@@ -18,3 +19,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+
+    def __str__(self):
+        return str(self.name)   
--- users/views.py
+++ users/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def profiles(request):
+    profiles = Profile.objects.all()
     return render(request, 'users/profiles.html')
