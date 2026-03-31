f3b6e1bcdd719bcfee1036b08fd2a855d7799781
Anatoliy Shekera <tolikshekera@gmail.com>
2026-03-31 22:32:45 +0300

    Profile view

--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
 
 def profiles(request):
     profiles = Profile.objects.all()
-    return render(request, 'users/profiles.html')
+    return render(request, 'users/profiles.html', {'profiles': profiles})
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    return render(request, 'users/user-profile.html', {'profile': profile})
