f21a0557b63187e0e4fcf6bb4df68b3a7c444beb
Anatoliy Shekera <tolikshekera@gmail.com>
2026-04-01 23:35:01 +0300

    User profile changes

--- users/views.py
+++ users/views.py
@@ -9,4 +9,6 @@ def profiles(request):
 
 def userProfile(request, pk):
     profile = Profile.objects.get(id=pk)
-    return render(request, 'users/user-profile.html', {'profile': profile})
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
+    return render(request, 'users/user-profile.html', {'profile': profile, 'topSkills': topSkills, 'otherSkills': otherSkills})
