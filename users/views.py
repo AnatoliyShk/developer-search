949ef1d04cf7efb3b247e4d2ff0ca7a082e602bc
Anatoliy Shekera <tolikshekera@gmail.com>
2026-04-14 00:44:42 +0300

    Signals fix

--- users/views.py
+++ users/views.py
@@ -5,22 +5,23 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 def loginUser(request):
     if request.user.is_authenticated:
         return redirect('profiles')
 
     if request.method == 'POST':
-        name = request.POST['name']
+        username = request.POST['username']
         password = request.POST['password']
 
         try:
-            user = User.objects.get(name=name)
+            user = User.objects.get(username=username)
         except:
             messages.error(request, 'User does not exist')
             user = None
 
-        user = authenticate(request, username=name, password=password)
+        user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
@@ -38,10 +39,10 @@ def registerUser(request):
     form = UserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.name = user.name.lower()
+            user.username = user.username.lower()
             user.save()
 
             messages.success(request, 'User account created successfully')
