ffdb68a7a30f1a6a7194a050ff3b2324b24af0ce
Anatoliy Shekera <tolikshekera@gmail.com>
2026-04-14 03:18:13 +0300

    login page styles

--- static/uikit/app.js
+++ static/uikit/app.js
@@ -1,4 +1,5 @@
-// Invoke Functions Call on Document Loaded
-document.addEventListener('DOMContentLoaded', function () {
-  hljs.highlightAll();
-});
+document.addEventListener('click', function (e) {
+  if (e.target.classList.contains('alert__close')) {
+    e.target.closest('.alert').style.display = 'none'
+  }
+})
