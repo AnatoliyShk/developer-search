3cec9c1088eeef84f7bca0f76a6765926f9fa421
Anatoliy Shekera <tolikshekera@gmail.com>
2026-04-18 23:28:30 +0300

    Projects utils

--- projects/views.py
+++ projects/views.py
@@ -8,10 +8,14 @@ def projects(request):
     search_query = ''
     if request.GET.get('search_query'):
         search_query = request.GET.get('search_query')
+
+    tags = Tag.objects.filter(name__icontains=search_query)
+
     projects = Project.objects.filter(
         Q(title__icontains=search_query) | 
         Q(description__icontains=search_query) |
-        Q(owner__name__icontains=search_query)
+        Q(owner__name__icontains=search_query) | 
+        Q(tags__in=tags)
     )
     title = "Projects"
     return render(request, 'projects/projects.html', {'projects': projects, 'title': title})
