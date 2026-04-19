a2068bcca89178cecfe2fb08d02da2dacd73c11f
Anatoliy Shekera <tolikshekera@gmail.com>
2026-04-20 01:11:58 +0300

    Django rest api

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes, name='routes'),
+    path('projects/', views.getProjects, name='projects'),
+    path('projects/<str:pk>/', views.getProject, name='project')
 ]
--- api/views.py
+++ api/views.py
@@ -2,7 +2,12 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from projects.models import Project, Review
+from .serializers import ProjectSerializer, ReviewSerializer
 
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         {'GET': '/api/projects'},
@@ -12,4 +17,16 @@ def getRoutes(request):
         {'POST': '/api/users/token'},
         {'POST': '/api/users/token/refresh/'}
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+@api_view(['GET'])
+def getProjects(request):
+    projects = Project.objects.all()
+    serializer = ProjectSerializer(projects, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getProject(request, pk):
+    project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
