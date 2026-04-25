a1052463ca6b3b4a5e6400e70b7518d18f3cab95
Anatoliy Shekera <tolikshekera@gmail.com>
2026-04-25 23:34:56 +0300

    Project tests

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
+from projects.models import Project
+
 # Create your tests here.
+
+class ProjectModelTests(TestCase):
+
+    def test_project_creation(self):
+        # Test that a project can be created successfully
+        project = Project.objects.create(name="Test Project")
+        assert project.id is not None
+        assert project.name == "Test Project"
