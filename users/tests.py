d54a9d6f2a1fd8283c896180ad84960f00364eb4
Anatoliy Shekera <tolikshekera@gmail.com>
2026-04-28 23:46:06 +0300

    Users test update

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+
+def test_user_creation():
+    # Test that a user can be created successfully
+    User = get_user_model()
+    user = User.objects.create_user(username="testuser", password="testpassword")
+    assert user.id is not None
+    assert user.username == "testuser"
+
+def test_user_str_representation():
+    # Test the string representation of a user
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    user = User.objects.create_user(username="testuser", password="testpassword")
+    assert str(user) == "testuser"
